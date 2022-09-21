3e6caa4e438be747245daf7423f7d15012292b10
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-09-21 12:44:18 +0100

    feat:passed tests

--- bubble-sort.js
+++ bubble-sort.js
@@ -7,12 +7,47 @@ function bubbleSort(arr) {
         // Swap those values
 
         // Do not move this console.log
-        console.log(arr.join(","));
-
+        
     // If you get to the end of the array and no swaps have occurred, return
 
     // Otherwise, repeat from the beginning
+  let len= arr.length;
 
+  for(let i=0; i<len; i++){
+    for(let j=0; j<len-1; j++){
+
+	  if(arr[j]> arr[j+1]){
+    let temp = arr[j];
+	   arr[j] = arr[j+1];
+        arr[j+1] = temp;
+   console.log(arr.join(",")); 
+
+          }
+    }
+  }
+
+
+
+/*
+	 let swaps = 0;
+
+	  do {
+	      // Iterate through the array
+		       swaps = 0;
+		           for (let i = 0; i < arr.length; i++) {
+		                 if (arr[i] > arr[i+1]) {
+		                         [arr[i+1], arr[i]] = [arr[i], arr[i+1]];
+		                                 swaps++;
+		                                         console.log(arr.join(","));
+		                                               
+		                                                   }
+		                                                     
+			   }
+	  } while (swaps > 0);
+		  
+
+	  return arr;
+*/
 }
 
 module.exports = bubbleSort;
